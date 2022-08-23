ffa2c21f78e7c33f9629f0f18d913b66f66682d8
Robert Ball <bobball89@gmail.com>
2022-08-23 11:54:23 -0700

    continued to update following in-class demo

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 
 console.log('This is my first server');
 
-const {request, response} = require('express');
+
 //servers we use require not import
 
 //Bring in express
 const express = require('express');
 require('dotenv').config();
+let data = require('./data/pets.json');
+const cors = require('cors');
 
 //once express is in we need to use it-per express docs
 const app = express();
+
+//middle ware to share resources across the internet
+app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
@@ -23,9 +28,26 @@ app.get('/', (request, response) =>{
 });
 
 app.get('/hello', (request, response) => {
-  response.status(200).send('This is the hello route');
+  console.log(request.query);
+  let firstName = request.query.firstName;
+  response.status(200).send(`Hello ${firstName}. This is the hello route`);
 });
 
+
+app.get('/pet', (request, response) => {
+  let species = request.query.species;
+  // console.log(species);
+  let dataToGroom = data.find(pet => pet.species === species);
+  let dataToSend = new Pet(dataToGroom);
+  response.status(200).send(dataToSend);
+});
+
+class Pet {
+  constructor(petObj){
+    this.name = petObj.name;
+    this.breed = petObj.breed;
+  }
+}
 
 //catch all that needs to be at the bottom. Sends message if user hits 404 error
 
